9a635f94a2cf2a0b17aa81f5038f113f8309e689
Manoel Carvalho <manoelmecat@gmail.com>
2021-02-23 20:42:17 -0300

    URL Postgres

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/pitu', {dialect: 'postgres'})
+const sequelize = new Sequelize('postgres://qsqtrorhrzzsgy:93dd52720f2f7a266486bdedfb4f431b5a7ad89e546f8bf04187b2a77dc36764@ec2-3-214-3-162.compute-1.amazonaws.com:5432/d67d8feuu66ali', {dialect: 'postgres'})
 
 export default sequelize
